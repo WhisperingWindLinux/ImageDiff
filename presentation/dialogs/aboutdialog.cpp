2f0a23ea4589565e71031c654e537c899ae8a0c8
WhisperingWindLinux <quid_lob.0o@icloud.com>
2025-03-24 15:50:09 +0600

    Increase version to 1.0.0-alpha

--- presentation/dialogs/aboutdialog.cpp
+++ presentation/dialogs/aboutdialog.cpp
@@ -16,7 +16,7 @@ AboutDialog::AboutDialog(QWidget *parent) : QDialog(parent) {
     appNameLabel->setAlignment(Qt::AlignCenter);
     layout->addWidget(appNameLabel);
 
-    QLabel *appVersion = new QLabel("version 0.5.1");
+    QLabel *appVersion = new QLabel("version 1.0.0-alpha");
     appVersion->setAlignment(Qt::AlignCenter);
     layout->addWidget(appVersion);
 
