253433297c0248c570eac723eb091b4d77bd2f43
WhisperingWindLinux <quid_lob.0o@icloud.com>
2025-04-01 16:33:50 +0600

    Added error in case of building under non-macOS OS

--- main.cpp
+++ main.cpp
@@ -7,6 +7,12 @@ int main(int argc, char *argv[]) {
 
     QApplication a(argc, argv);
 
+#ifndef TARGET_OS_MAC
+    #error The application was developed and tested for functionality only on macOS.
+    #error Functionality or even the possibility of building it for other operating systems is not guaranteed.
+    #error If you're feeling lucky, you can try to build and run the application on your chosen operating system.
+#endif
+
     MainWindow w;
     w.setWindowTitle("Image Diff");
     w.show();
